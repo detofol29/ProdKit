75fabb1e24d5aeb578a36ae9c3e29cdac69d5924
GabrielAugustoRamosD <gabriel.detofol@inventsoftware.com.br>
2025-06-26 17:59:42 -0300

    Adicionando interface

--- ProdKit.Application/Inferfaces/IConversorService.cs
+++ ProdKit.Application/Inferfaces/IConversorService.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace ProdKit.Application.Inferfaces
 {
-    internal interface IConversorService
+    public interface IConversorService
     {
+        void ConverterArquivo();
     }
 }
